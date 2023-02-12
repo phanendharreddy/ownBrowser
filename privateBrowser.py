b0e61bcdc7489615a51bff960df21f95c040a990
phanendharreddy <phanendharreddykusuma@gmail.com>
2023-02-13 00:06:54 +0530

    added new tab feature

--- privateBrowser.py
+++ privateBrowser.py
@@ -1,17 +1,27 @@
 import sys
-from PyQt5.QtCore import QUrl, Qt
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLineEdit, QVBoxLayout, QHBoxLayout, QAction, QToolBar, QWidget
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt5.QtCore import QUrl, Qt, QFile, QIODevice, QTextStream
+from PyQt5.QtWidgets import QApplication, QMainWindow, QTabWidget, QLineEdit, QVBoxLayout, QHBoxLayout, QAction, QToolBar, QWidget
+from PyQt5.QtWebEngineWidgets import QWebEngineView, QWebEngineProfile
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest
+
 
 class Browser(QMainWindow):
     def __init__(self):
         super().__init__()
         
-        self.setWindowTitle("PyQt5 Web Browser")
+        self.setWindowTitle("myBrowser")
         self.setGeometry(50,50,800,600)
         
-        self.web_view = QWebEngineView()
-        self.web_view.load(QUrl("http://www.google.com"))
+        # Enable extensions
+        profile = QWebEngineProfile.defaultProfile()
+        profile.setHttpUserAgent("Your custom User Agent string")
+        profile.setPersistentCookiesPolicy(QWebEngineProfile.AllowPersistentCookies)
+        
+        self.tabs = QTabWidget()
+        self.tabs.setTabsClosable(True)
+        self.tabs.tabCloseRequested.connect(self.close_tab)
+        
+        self.create_tab()
         
         self.address_bar = QLineEdit()
         self.address_bar.returnPressed.connect(self.load_url)
@@ -31,13 +41,17 @@ class Browser(QMainWindow):
         self.refresh_button = QAction("Refresh", self)
         self.refresh_button.triggered.connect(self.web_view.reload)
         self.navigation_bar.addAction(self.refresh_button)
+
+        self.newTabButton = QAction("New Tab", self)
+        self.newTabButton.triggered.connect(self.create_tab)
+        self.navigation_bar.addAction(self.newTabButton)
         
         self.load_button = QAction("Load", self)
         self.load_button.triggered.connect(self.load_url)
         self.navigation_bar.addAction(self.load_button)
         
         self.vbox_layout = QVBoxLayout()
-        self.vbox_layout.addWidget(self.web_view)
+        self.vbox_layout.addWidget(self.tabs)
         
         self.hbox_layout = QHBoxLayout()
         self.hbox_layout.addLayout(self.vbox_layout)
@@ -46,14 +60,25 @@ class Browser(QMainWindow):
         self.central_widget.setLayout(self.hbox_layout)
         self.setCentralWidget(self.central_widget)
         
+    def create_tab(self):
+        self.web_view = QWebEngineView()
+        self.web_view.load(QUrl("http://www.google.com"))
+        self.tabs.addTab(self.web_view, "New Tab")
+        self.tabs.setCurrentWidget(self.web_view)
+        
+    def close_tab(self, index):
+        self.tabs.removeTab(index)
+        
     def load_url(self):
         url = QUrl(self.address_bar.text())
-        self.web_view.load(url)
+        self.current_tab().load(url)
+        
+    def current_tab(self):
+        return self.tabs.currentWidget()
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     browser = Browser()
     browser.show()
     sys.exit(app.exec_())
-
 
